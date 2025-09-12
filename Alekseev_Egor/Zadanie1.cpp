4a000aedd20ae3cb988b0c9a90a0e1d09b36c1b0
alekseevegor07 <alekseevegor07@yandex.ru>
2025-09-12 10:24:38 +0300

    update

--- Alekseev_Egor/Zadanie1.cpp
+++ Alekseev_Egor/Zadanie1.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 typedef int RUB;
+typedef int PERCENT;
+
+PERCENT inflation = 7;
 
 struct Person {
     RUB bank_account;
@@ -11,6 +14,9 @@ struct Person {
     RUB tax;
     RUB fuel;
     RUB car_fix;
+    RUB trip;
+    RUB unexpected_costs;
+    RUB insurance;
     
 };
 
@@ -20,7 +26,7 @@ struct Person alice;
 void alice_income(const int year, const int month)
 {
     if(month == 9) {
-        alice.income = alice.income * 1.05;  // Indexation
+        alice.income = alice.income * 1.07;  // Indexation
     }
         
     if(year == 2030 && month == 3) {
@@ -34,11 +40,13 @@ void alice_income(const int year, const int month)
 void alice_food()
 {
     alice.bank_account -= alice.food;
+    alice.food *= (1+(inflation/100)/12);
 }
 
 void alice_clothes()
 {
     alice.bank_account -= alice.clothes;
+    alice.clothes *= (1+(inflation/100)/12);
 }
 
 void alice_mortgage()
@@ -49,17 +57,42 @@ void alice_mortgage()
 void alice_tax()
 {
     alice.bank_account -= alice.tax;
+    alice.tax *= (1+(inflation/100)/12);
 }
 
 void alice_fuel()
 {
     alice.bank_account -= alice.fuel;
+    alice.fuel *= (1+(inflation/100)/12);
 }
 
-void alice_car_fix(const int year, const int month)
+void alice_car_fix(const int month)
 {
     if(month == 6) {
         alice.bank_account -= alice.car_fix;  
+        alice.car_fix *= (1+(inflation/100));
+    }
+}
+
+void alice_trip(const int month)
+{
+    if(month == 8) {
+        alice.bank_account -= alice.trip;  
+        alice.trip *= (1+(inflation/100));
+    }
+}
+
+void alice_unexpected_costs()
+{
+    alice.bank_account -= alice.unexpected_costs;
+    alice.unexpected_costs *= (1+(inflation/100)/12);
+}
+
+void alice_insurance(const int month)
+{
+    if (month == 4){
+    alice.bank_account -= alice.insurance;
+    alice.insurance *= (1+(inflation/100));
     }
 }
 
@@ -75,9 +108,10 @@ void simulation()
         alice_mortgage();
         alice_tax();
         alice_fuel();
-        alice_car_fix(year, month);
-        // alice_trip();
-        
+        alice_insurance(month);
+        alice_car_fix(month);
+        alice_trip(month);
+        alice_unexpected_costs();
         
         
         ++month;
@@ -91,20 +125,23 @@ void simulation()
 
 void print_alice_info()
 {
-    printf("Alice capital = %d RUR\n", alice.bank_account);
+    printf("Alice capital = %d RUB\n", alice.bank_account);
 }
 
 
 void alice_init()
 {
     alice.bank_account = 1000 * 1000;
-    alice.monthly_payment = 112 * 1000;
+    alice.monthly_payment = 125 * 1000;
     alice.income = 200 * 1000;
     alice.food = 30000;
     alice.clothes = 1000;
     alice.tax = (20000)/12;
-    alice.fuel = 10000;
-    alice.car_fix = 30000;
+    alice.fuel = 15000;
+    alice.car_fix = 35000;
+    alice.trip = 200*1000;
+    alice.unexpected_costs = 10000;
+    alice.insurance = 40000;
 }
 
 
