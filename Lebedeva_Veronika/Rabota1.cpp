a7d76fca2dfd7c6b0b48b9cd13276da6bc450eb8
Veronika <l.veronika.cat@ya.ru>
2025-09-26 15:01:14 +0300

    Update Rabota1.cpp

--- Lebedeva_Veronika/Rabota1.cpp
+++ Lebedeva_Veronika/Rabota1.cpp
@@ -3,92 +3,109 @@
 #include <Windows.h>
 
 
-struct YEAR
+typedef int RUB;
+
+
+struct Date
 {
     int year;
     int month;
 };
 
 
-class Deposit                 
+class FinancialModelDeposit                 
 {
 private:
-    YEAR countperiod;
-    const YEAR data = {2025, 9};
-    YEAR end;
+    Date countperiod;
+    const Date data = {2025, 9};
+    Date current;
+    RUB salary = 220000;
+    RUB monthflat = 50000;
+    RUB foodmonth = 30000;
 public:
-    int startcap = 2188890; 
+    RUB startcap = 2188890;
     int years = 20;
     float percent = 15.5f;
-    int sum = startcap;
-    
-    void incrofmon();
-    void show_inf();
+    RUB bank_account = startcap;
+    RUB money_on_card = 0;
+    void Salary(Date Cur);
+    void Food(Date Cur);
+    void Flat(Date Cur);
+    void increase_of_money();
+    void show_results();
+    void deposit();
 };
+ 
 
-
-void expences(const YEAR& start, int Months, int& Sum)
+void FinancialModelDeposit::Salary(Date Cur)
 {
-    int exp = 0;
-    YEAR current = start;
-    static int salary = 220000;
-    static int foodmonth = 30000;
-    static int monthflat = 50000;
-    for (int i = 0; i < Months; i++) {
-        if (current.month == 10) {
-            salary = static_cast<int>(salary * 1.07);
+    if (Cur.month == 10) {
+        salary = static_cast<int>(salary * 1.07);
+    }
+    money_on_card += salary;
+}
+
+
+void FinancialModelDeposit::Food(Date Cur)
+{
+    if (Cur.year % 2 == 0 && Cur.month == 9) {
+        foodmonth = static_cast<int>(foodmonth * 1.13);
+    }
+    money_on_card -= foodmonth;
+}
+
+
+void FinancialModelDeposit::Flat(Date Cur)
+{
+    if (Cur.year % 3 == 0 && Cur.month == 9 && Cur.year != 2025) {
+        monthflat = static_cast<int>(monthflat * 1.33);
+    }
+    money_on_card -= monthflat;
+}
+
+
+void FinancialModelDeposit::deposit()
+{
+    Date period;
+    period.month = 3;
+    if (current.month % period.month == 0 && (current.month != 0)) {
+        double quartalpercent = percent / 4.0 / 100.0;
+        RUB afterpercent = bank_account * quartalpercent;
+        bank_account += afterpercent;
+        float cbrate = (percent + 1.5) / 100; //central bank rate
+        RUB nontax = 1000000 * cbrate;
+        if (afterpercent > nontax){
+            bank_account = static_cast<int>(bank_account - 0.13 * (afterpercent - nontax));
         }
-        if (current.year % 2 == 0 && current.month == 9) {
-            foodmonth = static_cast<int>(foodmonth * 1.13);
-        }
-        if (current.year % 3 == 0 && current.month == 9 && current.year != 2025) {
-            monthflat = static_cast<int>(monthflat * 1.33);
-        }
-        exp += salary - foodmonth - monthflat;
-        std::cout << current.year << " : " << current.month << " - exp = " << exp << std::endl;
+        percent -= 0.1f;
+        std::cout << current.year << " : " << current.month << " - " << bank_account << std::endl;
+    }
+}
+
+
+void FinancialModelDeposit::increase_of_money()    
+{
+    current.month = data.month;
+    current.year = data.year;
+    while (!(current.year == (data.year + years) && current.month == data.month)) {
+        deposit();
+        Salary(current);
+        Food(current);
+        Flat(current);
+        bank_account += money_on_card;
+        money_on_card = 0;
         current.month++;
         if (current.month > 12) {
             current.month = 1;
             current.year++;
-        }
-    }
-    Sum += exp;
-    std::cout << current.year << " : " << current.month << " - Sum = " << Sum << std::endl;
-}
-
-
-void Deposit::incrofmon()     //increase of money
-{
-    end.month = data.month;
-    end.year = data.year;
-    while (!(end.year == (data.year + years) && end.month == data.month)){
-        YEAR period;
-        period.month = 3;
-        if (end.month % 3 == 0 && (end.month != 0)){
-            double quartalpercent = percent / 4.0 / 100.0;
-            int afterpercent = sum * quartalpercent;
-            sum += afterpercent;
-            float cbrate = (percent + 1.5) / 100; //central bank rate
-            int nontax = 1000000 * cbrate;
-            if (afterpercent > nontax){
-                sum = static_cast<int>(sum - 0.13 * (afterpercent - nontax));
-            }
-            percent -= 0.1f;
-            std::cout << end.year << " : " << end.month << " - " << sum << std::endl;
-            expences(end, 3, sum);
-        }
-        end.month++;
-        if (end.month > 12){
-            end.month = 1;
-            end.year++;
-        }
+        } 
     }
 }
 
 
-void Deposit::show_inf()
+void FinancialModelDeposit::show_results()
 {
-    std::cout << "общий доход со вклада за " << years << " лет составил " << sum << " рублей" << std::endl;
+    std::cout << "общий доход со вклада за " << years << " лет составил " << bank_account << " рублей" << std::endl;
 }
 
 
@@ -97,7 +114,7 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "Russian");
-    Deposit Maria;
-    Maria.incrofmon();
-    Maria.show_inf();
+    FinancialModelDeposit Maria;
+    Maria.increase_of_money();
+    Maria.show_results();
 }
