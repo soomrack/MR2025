5b5d26e9d0326b727e4d0b07e6f1c20b6e4699eb
gigabebra <147887321+gigabebra@users.noreply.github.com>
2025-09-29 13:15:35 +0300

    Create test.c

---
+++ Dunaev_Alexander/src/test.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+//TODO добавить reverse строки перед for
+
+void print_dots(const int number){
+    char str[20];
+    sprintf(str,"%d", number);
+    for(int j=20;j>=0;--j){
+        if(str[j]=='\0'){continue;}
+        putchar(str[j]);
+        if(j%3==0){
+            putchar('.');
+        }
+    } 
+    putchar('\n');
+    printf("%d\n",(sizeof(str)/sizeof(str[0])));
+    printf("%s\n",str);
+
+}
+
+int main(void){
+
+
+    print_dots(100000000);
+
+    return 0;
+}
