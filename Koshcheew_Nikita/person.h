89d6f1c9e521b390219862ad6d0fe7c9cadc2a01
kibberpank <102148856+kibberpank@users.noreply.github.com>
2025-09-29 12:09:24 +0300

    Update person.h

--- Koshcheew_Nikita/person.h
+++ Koshcheew_Nikita/person.h
@@ -4,18 +4,27 @@ typedef long long RUB;
 
 class Person {
 public:
-	Person(RUB income, RUB deposit, RUB credit = 0);
-	void printInfo();
-	void setCreditRate(int rate);
-	void increaseCredit();
-	void increaseDeposit(int percent);
-	void increaseSalary(int inflation);
-	void getAndSpendMoney(RUB regularSpend, RUB rent = 0);
-
+	explicit Person(RUB income);
+	void get_dep_percent(int inflation);
+	void increase_salary(int inflation);
+	void set_food_spending(RUB prices);
+	void set_mortgage_pay(RUB pay);
+	void set_rent(RUB rent);
+	void income();
+	void buy_food();
+	void pay_rent();
+	void pay_mortgage();
+	void increase_prices(int inflation);
+	void buy_flat(RUB cost_of_flat);
+	void print_info();
 private:
-	RUB m_deposit;
-	RUB m_credit;
+	RUB m_deposit{0};
 	RUB m_income;
-	int m_creditRate;
+	RUB m_food{0};
+	RUB m_rent{0};
+	RUB m_mortgage_pay{ 0 };
+
+	void increase_food_spending(int inflation);
+	void increase_rent(int inflation);
 };
 
