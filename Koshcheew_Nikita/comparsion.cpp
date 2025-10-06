1081fa55a62baa7f184961bb271eea30f4da6fb1
kibberpank <102148856+kibberpank@users.noreply.github.com>
2025-10-06 12:57:05 +0300

    Update comparsion.cpp

--- Koshcheew_Nikita/comparsion.cpp
+++ Koshcheew_Nikita/comparsion.cpp
@@ -10,17 +10,19 @@ struct Date {
 };
 
 
-void set_random_inflation(int& inflation) {
+void set_random_inflation(int month, int& inflation) {
     static std::random_device rd;
     static std::mt19937 gen(rd());
     static std::uniform_int_distribution<> distrib(1, 9);
-    inflation = distrib(gen);
-    std::cout << inflation << '\n';
+    if (month == 1) {
+        inflation = distrib(gen);
+        std::cout << inflation << '\n';
+    }
 }
 
 
-void increase_price(RUB& price, int inflation) {
-    price *= ((100.0 + inflation) / 100.0);
+void increase_price(int month, RUB& price, int inflation) {
+    if (month == 1) price *= ((100.0 + inflation) / 100.0);
 }
 
 
@@ -33,44 +35,83 @@ RUB get_mortgage_pay(RUB cost_of_flat, int inflation, int years) {
 }
 
 
-void simulation(Person& alice, Person& bob, int startingInflation) {
-    int inflation{ startingInflation };
-    RUB cost_of_flat{ 12500000 };
+void alice_init(Person& alice, RUB cost_of_flat, int inflation) {
     RUB mortgage_pay = get_mortgage_pay(cost_of_flat, inflation, 20);
     alice.set_mortgage_pay(mortgage_pay);
-    
-    Date date{ 2025, 9 };
 
     alice.set_food_spending(30000);
     alice.set_communal(10000);
+}
 
+
+void bob_init(Person& bob) {
     bob.set_food_spending(30000);
     bob.set_communal(10000);
     bob.set_rent(45000);
+}
+
+
+void car_cost_amortisation(RUB& cost_of_car, int month) {
+    if (month == 12) cost_of_car *= 0.97;
+}
+
+
+void bob_car_ownership(Person& bob, RUB& cost_of_car, Date date, int inflation) {
+    static bool have_car{ false };
+    static RUB spending_on_car{ 20000 };
+
+    if (date.year == 2010 && date.month == 3) {
+        bob.buy_car(cost_of_car);
+        have_car = true;
+    }
+
+    increase_price(date.month, spending_on_car, inflation);
+
+    if (have_car) {
+        car_cost_amortisation(cost_of_car, date.month);
+        bob.spend_money_on_car(spending_on_car);
+    }
+}
+
+
+void simulation(Person& alice, Person& bob, int starting_inflation) {
+    int inflation{ starting_inflation };
+    RUB cost_of_flat{ 12500000 };
+    RUB cost_of_car{ 1000000 };
+
+    alice_init(alice, cost_of_flat, inflation);
+    bob_init(bob);
+
+    Date date{ 2025, 9 };
 
     while (!(date.year == 2045 && date.month == 8)) {
         alice.get_income(date.month, inflation);
         alice.spend_money(date.month, inflation);
+        //регулярные траты на еду, коммуналку и ипотеку
         alice.put_money_on_deposit();
         alice.get_dep_percent(inflation);
 
         bob.get_income(date.month, inflation);
         bob.spend_money(date.month, inflation);
+        //регулярные траты на еду, коммуналку и аренду
+        bob_car_ownership(bob, cost_of_car, date, inflation);
         bob.put_money_on_deposit();
         bob.get_dep_percent(inflation);
 
         date.month += 1;
-
         if (date.month == 13) {
             date.month = 1;
-            date.year += 1;
-
-            increase_price(cost_of_flat, inflation);
-            set_random_inflation(inflation);
+            date.year += 1;   
         }
+
+        increase_price(date.month, cost_of_flat, inflation);
+        increase_price(date.month, cost_of_car, inflation);
+
+        set_random_inflation(date.month, inflation);
     }
 
-    bob.buy_flat(cost_of_flat);
+    alice.print_info_alice(cost_of_flat);
+    bob.print_info_bob(cost_of_car);
 }
 
 
@@ -79,13 +120,10 @@ int main() {
     Person Alice(300000);
     Person Bob(300000);
 
-    int startingInflation;
-    std::cin >> startingInflation;
+    int starting_inflation;
+    std::cin >> starting_inflation;
 
-    simulation(Alice, Bob, startingInflation);
-
-    Alice.print_info();
-    Bob.print_info();
+    simulation(Alice, Bob, starting_inflation);
 
     return 0;
 }
