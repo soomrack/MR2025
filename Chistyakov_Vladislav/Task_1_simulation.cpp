f34054c466a41734d8f6d305c1f5ae88e72774d0
Zefri0n <Vladran2@mail.ru>
2025-10-08 16:05:54 +0300

    5th upload
    finish

--- Chistyakov_Vladislav/Task_1_simulation.cpp
+++ Chistyakov_Vladislav/Task_1_simulation.cpp
@@ -7,28 +7,22 @@ typedef long long int RUB;
 std::mt19937 mt{ std::random_device{}() };
 
 struct apartment {
-	RUB cost;               // Цена квартиры
-
+	RUB cost;               // Цена имеющейся квартиры
+	RUB desired_cost;            // Цена желаемой квартиры
 	RUB rent;               // Аренда квартиры
 	RUB utilities;          // Коммунальные услуги
-
-	bool HasApartment;
 };
 
 struct bank{
 	RUB account;            // Счёт в банке
-
 	RUB mortgage_amount;    // Остаток суммы по ипотеке
 	RUB mortgage_payment;   // Ежемесячный платёж по ипотеке
-
 	RUB investment;         // Вклад в банке
 };
 
 struct person {
 	RUB income;          // Зарплата
-
 	RUB food;            // Траты на еду
-
 	apartment apartment;
 	bank bank;
 };
@@ -49,7 +43,6 @@ void alice_init() {
 	alice.bank.mortgage_amount = 8 * 1000 * 1000;
 	alice.bank.mortgage_payment = mortgage_payment(alice.bank.mortgage_amount, 0.06, 12 * 20); // 6%, на 20 лет
 	alice.apartment.cost = 10 * 1000 * 1000;
-	alice.apartment.HasApartment = true;
 	alice.apartment.utilities = 4 * 1000;
 	alice.food = 20 * 1000;
 }
@@ -59,8 +52,8 @@ void bob_init() {
 	bob.income = 100 * 1000;
 	bob.bank.account = 0;
 	bob.bank.investment = 2 * 1000 * 1000;
-	bob.apartment.cost = 10 * 1000 * 1000;
-	bob.apartment.HasApartment = false;
+	bob.apartment.desired_cost = 10 * 1000 * 1000;
+	bob.apartment.cost = 0;
 	bob.apartment.rent = 30 * 1000;
 	bob.apartment.utilities = 4 * 1000;
 	bob.food = 20 * 1000;
@@ -86,14 +79,7 @@ void alice_food(const int& month) {
 }
 
 void alice_mortgage(const int& year, const int& month) {
-	if (alice.bank.mortgage_amount > 0) {
-		alice.bank.mortgage_amount -= alice.bank.mortgage_payment;
-		alice.bank.account -= alice.bank.mortgage_payment;
-	}
-	else if (alice.bank.mortgage_amount != 0) {
-		std::cout << "Алиса закрыла ипотеку: " << month << " месяц " << year << " год\n";
-		alice.bank.mortgage_amount = 0;
-	}
+	alice.bank.account -= alice.bank.mortgage_payment;
 }
 
 void alice_utilities(const int& month) {
@@ -111,20 +97,24 @@ void alice_rand_expenses() {
 void alice_investment(const int& year, const int& month) {
 	alice.bank.investment *= 1.0 + 0.20 / 12;              // Начисление процетов на вклад в банке под 20% годовых (каждый месяц)
 
-	alice.bank.investment += alice.bank.account - 10 * 1000;
-	alice.bank.account == 10 * 1000;
+	alice.bank.investment += alice.bank.account - 10000;
+	alice.bank.account == 10000;
 }
 
 
 void bob_income(const int& year, const int& month) {
 	if (month == 10) bob.income *= 1.07;                   // Учёт инфляции на зарплату
 	if (year == 2030 && month == 3) bob.income *= 1.5;     // Повышение в должности
+
 	bob.bank.account += bob.income;
 }
 
 
 void bob_apartment(const int& month) {
-	if (month == 10) bob.apartment.cost *= 1.07;
+	if (month == 10) {
+		bob.apartment.cost *= 1.07;
+		bob.apartment.desired_cost *= 1.07;
+	}
 }
 
 
@@ -140,7 +130,7 @@ void bob_utilities(const int& month) {
 		bob.apartment.utilities *= 1, 07;
 	}
 
-	if (!bob.apartment.HasApartment) bob.bank.account -= bob.apartment.rent;    // Пока нет квартиры - арендует
+	if (bob.apartment.cost == 0) bob.bank.account -= bob.apartment.rent;    // Пока нет квартиры - арендует
 	else bob.bank.account -= bob.apartment.utilities;                           // С квартирой платит комунальные услуги
 }
 
@@ -157,9 +147,9 @@ void bob_investment(const int& year, const int& month) {
 	bob.bank.investment += alice.bank.account;
 	alice.bank.account == 0;
 
-	if (!bob.apartment.HasApartment && bob.bank.investment > bob.apartment.cost) {       // Если хватает инвестиций снимает и покупает
-		bob.bank.investment -= bob.apartment.cost;
-		bob.apartment.HasApartment = true;
+	if (bob.apartment.cost == 0 && bob.bank.investment >= bob.apartment.desired_cost) {       // Если хватает инвестиций снимает и покупает
+		bob.bank.investment -= bob.apartment.desired_cost;
+		bob.apartment.cost = bob.apartment.desired_cost;
 		std::cout << "Боб покупает квартиру: " << month << " месяц " << year << " год\n";
 	}
 }
@@ -196,15 +186,15 @@ void simulation() {
 
 
 void alice_print() {
-	std::cout << "Счёт Алисы в банке: " << /*alice.bank.account + alice.apartment.cost + */alice.bank.investment << "\n";
-	if (alice.apartment.HasApartment) std::cout << "Есть квартира\n";
+	std::cout << "Счёт Алисы в банке: " << alice.bank.account + alice.apartment.cost + alice.bank.investment << "\n";
+	if (alice.apartment.cost != 0) std::cout << "Есть квартира\n";
 	else std::cout << "Нет квартиры\n";
 }
 
 
 void bob_print() {
 	std::cout << "Счёт Боба в банке: " << bob.bank.account + bob.apartment.cost + bob.bank.investment << "\n";
-	if (bob.apartment.HasApartment) std::cout << "Есть квартира\n";
+	if (bob.apartment.cost != 0) std::cout << "Есть квартира\n";
 	else std::cout << "Нет квартиры\n";
 }
 
