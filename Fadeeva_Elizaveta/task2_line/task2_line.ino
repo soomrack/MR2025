23dae3db40ae69ae2b84017faeceafb30ac0141c
Lizaveta-han <lizavetafadeeva.lizaf@gmail.com>
2025-10-13 12:30:14 +0300

    Update task2_line.ino
    Errors correction

--- Fadeeva_Elizaveta/task2_line/task2_line.ino
+++ Fadeeva_Elizaveta/task2_line/task2_line.ino
@@ -4,15 +4,15 @@
 #define MOTOR_R_PWM 5
 #define MOTOR_R_DIR 4
 #define BUTTON_PIN 12
-#define SENSOR_L A0
-#define SENSOR_R A1
+#define SENSOR_L A1
+#define SENSOR_R A0
 
 // Коэффициенты ПД-регулятора
-float Kp = 7.4;
-float Kd = 4.7;
-int baseSpeed = 170;
+float Kp = 3.0; 
+float Kd = 1.2; 
+int baseSpeed = 130;  
 
-// Переменные для калибровки 
+// Переменные калибровки
 int l_white, r_white, l_black, r_black;
 int l_threshold, r_threshold;
 int l_minVal = 1023, l_maxVal = 0;
@@ -31,16 +31,16 @@ void setMotors(int left, int right) {
     analogWrite(MOTOR_R_PWM, constrain(abs(right), 0, 255));
 }
 
-// Усреднённое считывание аналогового сигнала 
+// Усреднённое считывание сигнала
 int readSmooth(int pin) {
     long total = 0;
-    for (int i = 0; i < 15; i++) total += analogRead(pin);
-    return total / 15;
+    for (int i = 0; i < 10; i++) total += analogRead(pin);
+    return total / 10;
 }
 
-// Калибровка датчиков 
+// Калибровка
 void calibrate() {
-    Serial.println("Calibration of sensors...");
+    Serial.println("Calibration...");
     while (digitalRead(BUTTON_PIN)) delay(10);
     while (!digitalRead(BUTTON_PIN)) delay(10);
 
@@ -58,14 +58,11 @@ void calibrate() {
     r_minVal = r_black;
     r_maxVal = r_white;
 
-    float ratio = 0.1; // 10% порог
+    float ratio = 0.3; // 30% порог, более "мягкий"
     l_threshold = l_black + (l_white - l_black) * ratio;
     r_threshold = r_black + (r_white - r_black) * ratio;
 
-    Serial.println("Calibration completed:");
-    Serial.print("L: "); Serial.print(l_minVal); Serial.print(" - "); Serial.println(l_maxVal);
-    Serial.print("R: "); Serial.print(r_minVal); Serial.print(" - "); Serial.println(r_maxVal);
-    Serial.print("Thresholds: "); Serial.print(l_threshold); Serial.print(" / "); Serial.println(r_threshold);
+    Serial.println("Calibration done");
 }
 
 // Проверка потери линии
@@ -75,13 +72,13 @@ bool lineLost() {
     return (l_read < l_threshold && r_read < r_threshold);
 }
 
-// Поиск линии, если она потеряна 
+// Поиск линии
 void recoverLine() {
     for (int k = 0; k < 3; k++) {
         if (lastError > 0)
-            setMotors(80, -80);
+            setMotors(70, -70);
         else if (lastError < 0)
-            setMotors(-80, 80);
+            setMotors(-70, 70);
         else
             setMotors(-60, -60);
 
@@ -91,13 +88,16 @@ void recoverLine() {
     }
 }
 
-//Следование по линии
+// Следование по линии
 void followTrack() {
-    int l_val = map(analogRead(SENSOR_L), l_minVal, l_maxVal, 0, 100);
-    int r_val = map(analogRead(SENSOR_R), r_minVal, r_maxVal, 0, 100);
+    int l_val = map(readSmooth(SENSOR_L), l_minVal, l_maxVal, 0, 100);
+    int r_val = map(readSmooth(SENSOR_R), r_minVal, r_maxVal, 0, 100);
 
     float error = l_val - r_val;
     float correction = Kp * error + Kd * (error - lastError);
+
+    // Ограничим коррекцию, чтобы не крутился на месте
+    correction = constrain(correction, -baseSpeed, baseSpeed);
 
     int leftPower = baseSpeed + correction;
     int rightPower = baseSpeed - correction;
@@ -109,7 +109,6 @@ void followTrack() {
 // Инициализация
 void setup() {
     Serial.begin(9600);
-
     pinMode(MOTOR_L_PWM, OUTPUT);
     pinMode(MOTOR_L_DIR, OUTPUT);
     pinMode(MOTOR_R_PWM, OUTPUT);
@@ -122,18 +121,21 @@ void setup() {
     systemActive = true;
 }
 
-// Основной цикл
+// Основной цикл 
 void loop() {
     bool btnState = digitalRead(BUTTON_PIN);
 
     if (btnState == LOW && lastButtonState == HIGH) {
         systemActive = !systemActive;
-        delay(80);
+        delay(100);
     }
     lastButtonState = btnState;
 
     if (systemActive) {
         if (lineLost()) recoverLine();
         else followTrack();
+    }
+    else {
+        setMotors(0, 0);
     }
 }
