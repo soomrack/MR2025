c76f7a8d62868ba3de1a3e2abdb3ca29222b4756
sonechkq <90902767+sonechkq@users.noreply.github.com>
2025-10-21 20:21:27 +0300

    Task1_BobAlice_v2

--- Paritckaya_Sofia/Task1_BobAlice.cpp
+++ Paritckaya_Sofia/Task1_BobAlice.cpp
@@ -7,12 +7,16 @@ typedef long long int Money; // RUB
 typedef struct {
     Money savings;
     Money salary;
-    Money monthly_fee;
-    Money utility_expenses; // КУ
+    typedef struct {
+        Money monthly_fee; // ипотечный платеж
+        Money utility_expenses; // КУ
+        Money property_value; // стоимость жилья
+        Money rent; // аренда жилья для Боба
+    } Home; // структура в структуре - объединены расходы на жилье
     Money food_expenses; 
     Money other_expenses;
-    Money property_value; // стоимость недвижимости
     Money expenses;
+    Home home;
 } Person;
 
 bool bob_buy = false; // Боб купил квартиру
@@ -24,9 +28,9 @@ static void alice_init()
 {
     alice.savings = 0;
     alice.salary = 220 * 1000;
-    alice.monthly_fee = 130 * 1000;  // ежемесячная плата по ипотеке
-    alice.property_value = 9610579;
-    alice.utility_expenses = 5000;
+    alice.home.monthly_fee = 130 * 1000;
+    alice.home.property_value = 9610579;
+    alice.home.utility_expenses = 5000;
     alice.food_expenses = alice.salary * 0.1;
     alice.other_expenses = alice.salary * 0.05;
     alice.expenses = 0;
@@ -37,9 +41,9 @@ static void bob_init()
 {
     bob.savings = 2000 * 1000;
     bob.salary = 220 * 1000;
-    bob.monthly_fee = 42 * 1000;  // ежемесячная плата за аренду квартиры
-    bob.property_value = 0;
-    bob.utility_expenses = 5000;
+    bob.home.rent = 42 * 1000;
+    bob.home.property_value = 0;
+    bob.home.utility_expenses = 5000;
     bob.food_expenses = bob.salary * 0.1;
     bob.other_expenses = bob.salary * 0.05;
     bob.expenses = 0;
@@ -59,10 +63,10 @@ static void alice_salary(const int month)
 static void alice_expenses(const int month)
 {
 
-    alice.expenses = alice.utility_expenses + alice.food_expenses + alice.other_expenses + alice.monthly_fee;
+    alice.expenses = alice.home.utility_expenses + alice.food_expenses + alice.other_expenses + alice.home.monthly_fee;
 
     if (month == 12) {
-        alice.utility_expenses *= 1.07;
+        alice.home.utility_expenses *= 1.07;
         alice.food_expenses *= 1.07;
         alice.other_expenses *= 1.07;
     }
@@ -79,7 +83,7 @@ static void alice_savings(const int month)
     }
 
     if (month == 12) {
-        alice.property_value *= 1.02;
+        alice.home.property_value *= 1.02;
     }
 }
 
@@ -94,22 +98,21 @@ static void bob_salary(const int month)
 }
 
 
-static void bob_expenses(const int month)
+static void bob_expenses(const int year, const int month)
 {
     if (bob_buy == false) {
-        bob.expenses = bob.utility_expenses + bob.food_expenses + bob.other_expenses + bob.monthly_fee;
+        bob.expenses = bob.home.utility_expenses + bob.food_expenses + bob.other_expenses + bob.home.rent;
     }
     else {
-        bob.expenses = bob.utility_expenses + bob.food_expenses + bob.other_expenses;
+        bob.expenses = bob.home.utility_expenses + bob.food_expenses + bob.other_expenses;
     }
 
     if (month == 12) {
-        bob.utility_expenses *= 1.07;
+        bob.home.utility_expenses *= 1.07;
         bob.food_expenses *= 1.07;
         bob.other_expenses *= 1.07;
-        bob.monthly_fee *= 1.03;
+        bob.home.rent *= 1.03;
     }
-
 }
 
 
@@ -122,28 +125,28 @@ static void bob_savings(const int month)
     }
 
     if (month == 12) {
-        bob.property_value *= 1.02;
+        bob.home.property_value *= 1.02;
     }
 }
 
 static void bob_buy_home(const int year, const int month)
 {
-    if (bob.savings >= alice.property_value && bob_buy == false) {
+    if (bob.savings >= alice.home.property_value && bob_buy == false) {
         printf("Bob buy home date = %lld, %lld \n", year, month);
-        bob.savings -= alice.property_value;
-        bob.property_value = alice.property_value;
+        bob.savings -= alice.home.property_value;
+        bob.home.property_value = alice.home.property_value;
         bob_buy = true;
     }
 }
 
 
 static void print_info_alice() {
-    printf("Alice capital = %lld RUB\n", alice.savings + alice.property_value);
+    printf("Alice capital = %lld RUB\n", alice.savings + alice.home.property_value);
 }
 
 
 static void print_info_bob() {
-    printf("Bob capital = %lld RUB\n", bob.savings + bob.property_value);
+    printf("Bob capital = %lld RUB\n", bob.savings + bob.home.property_value);
 }
 
 
@@ -159,7 +162,7 @@ static void simulation()
         alice_savings(month);
 
         bob_salary(month);
-        bob_expenses(month);
+        bob_expenses(year, month);
         bob_savings(month);
         bob_buy_home(year, month);
 
