bf3b7aac5adce49b41b7ce153b2eefee410c3994
viskasss27 <vika27vika25@gmail.com>
2025-10-24 10:52:32 +0300

    alice_bob.cpp
    Добавились накопления

--- Saikinova_Victoria/alice.cpp
+++ Saikinova_Victoria/alice.cpp
@@ -1,43 +1,55 @@
-#include <stdio.h>
 #include <cmath>
+#include <cstdio>
 
 typedef long long int RUB;
 
 struct person {
     RUB salary;
     RUB balance;
-    RUB cost;
     RUB payment;
+    RUB savings;
 };
 
 person alice;
 person bob;
 
-RUB first_flat_cost;
+RUB remittance;
 RUB second_flat_cost;
 
+RUB food = 10 * 1000;
+RUB cosmetic = 3 * 1000;
+RUB cat_food = 2 * 1000;
+RUB communal_flat = 5 * 1000;
+RUB clothes = 5 * 1000;
+RUB relaxation = 15 * 1000;
+RUB self_care = 4 * 1000;
+RUB dog_food = 3 * 1000;
+RUB dog_hospital = 5 * 1000;
+RUB computer_game = 1000;
+RUB car = 20 * 1000;
+RUB hospital = 5 * 1000;
 
-void init(RUB salary, RUB balance) {
+void init(RUB salary, RUB savings) {
     alice.salary = salary;
-    alice.balance = balance;
+    alice.savings = savings;
     bob.salary = salary;
-    bob.balance = balance;
+    bob.savings = savings;
 }
 
 
 void print1() {
     printf("ALice took out a mortgage\n");
     printf("ALice has salary = %lld RUB\n", alice.salary);
-    printf("ALice has balance = %lld RUB\n\n", alice.balance);
+    printf("ALice has savings = %lld RUB\n\n", alice.savings);
 
     printf("Bob rented an apartment\n");
     printf("Bob has salary = %lld RUB\n", bob.salary);
-    printf("Bob has balance = %lld RUB\n\n", bob.balance);
+    printf("Bob has savings = %lld RUB\n\n", bob.savings);
 }
 
 
 void print2() {
-    printf("\nALice has balance = %lld RUB\n", alice.balance);
+    printf("\nALice has savings = %lld RUB\n", alice.savings);
     if (alice.balance < 0) {
         printf("ALice starves(\n");
     }
@@ -45,112 +57,128 @@ void print2() {
         printf("ALice`s happy)\n");
     }
 
-    printf("\nBob has balance = %lld RUB\n", bob.balance);
-    if (bob.balance < 20 * 1000 * 1000) {
-        printf("Bob starves( \n", bob.balance);
+    printf("\nBob has savings = %lld RUB\n", bob.savings);
+    if (bob.savings < 50 * 1000 * 1000) {
+        printf("Bob starves( \n");
     }
     else printf("Bob will buy an apartment)\n");
 }
 
 
 void inflation_do(const int year, const int month, double inflation) {
-    if ((year - 2025) % 5 == 0 && month == 9 && year != 2025) {
-        alice.salary += 10 * 1000;
-        bob.salary += 13 * 1000;
-        /*printf("salary %lld \n", alice.salary);*/
-    }
     if (month == 12) {
-        alice.salary = RUB (double (alice.salary) * (inflation - 0.1));
-        bob.salary = RUB (double(bob.salary) * (inflation - 0.1));
-     
-        alice.cost *= inflation;
-        bob.cost *= inflation;
-    }
-    if (month == 12) {
-        
-    }
+        alice.salary = RUB(double(alice.salary) * (inflation - 0.1));
+        bob.salary = RUB(double(bob.salary) * (inflation - 0.1));
 
+        food *= inflation;
+        cosmetic *= inflation;
+        cat_food *= inflation;
+        communal_flat *= inflation;
+        clothes *= inflation;
+        relaxation *= inflation;
+        self_care *= inflation;
+        dog_food *= inflation;
+        dog_hospital *= inflation;
+        computer_game *= inflation;
+        car *= inflation;
+        hospital *= inflation;
+    
+    }
 }
+
+
+//                  ALICE                  //
 
 
 void payment_mortgage(RUB amount_mortgage, double mortgage_percentage) {
     double monthly_rate = mortgage_percentage / 12;
     RUB remaining_mortgage = amount_mortgage - 100 * 1000;
-    alice.payment = RUB( double(remaining_mortgage) * (monthly_rate * pow((1 + monthly_rate),20) ) );
+    alice.payment = RUB(double(remaining_mortgage) * (monthly_rate * pow((1 + monthly_rate), 20)));
     /*printf("payment %lld \n",payment);*/
 }
 
 
-void payment_rent_flat(RUB first_flat, RUB second_flat) {
-    first_flat_cost = first_flat;
-    second_flat_cost = second_flat;
-}
-;
-
-
-void payment_rent(const int year) {
-    if (year < 2035) bob.payment = first_flat_cost;
-    else bob.payment = second_flat_cost;
-}
-
-void expenditure() {
-    RUB food = 10 * 1000;
-    RUB cosmetic = 1000;
-    RUB cat = 2 * 1000;
-    RUB communal_flat = 5 * 1000;
-    RUB clothes = 3 * 1000;
-    RUB relaxation = 5 * 1000;
-    RUB self_care = 4 * 1000;
-    RUB dog = 5 * 1000;
-    RUB computer_game = 1000;
-    RUB car = 4 * 1000;
-    alice.cost = food + cosmetic + cat + communal_flat + clothes + relaxation + self_care;
-    bob.cost = food + car + communal_flat + clothes / 2 + computer_game + relaxation/2 + dog;
+void alice_payment() {
+    alice.balance -= alice.payment;
 }
 
 
-void calculate(double inflation) {
-    int month = 9;
-    int year = 2025;
+void alice_salary(const int month, const int year) {
+    alice.balance += alice.salary;
+    if ((year - 2025) % 5 == 0 && month == 9 && year != 2025) {
+        alice.salary += 30 * 1000;
 
-    expenditure();
-
-    while (!(month == 9 && year == 2045)) {
-
-        alice.balance += alice.salary - alice.payment - alice.cost;
-        bob.balance += bob.salary - bob.payment - bob.cost;
-
-        inflation_do(year, month, inflation);
-
-        if (alice.balance < 0) printf("alice need a part-time job %lld, month %lld, year %lld\n",
-                    alice.balance, month, year);
-
-        month ++;
-        if (month == 13) {
-            year++;
-            month = 1;
-        }
     }
 }
 
 
-int main() {
-    init(120*1000, 580*1000);
-    print1();
-    payment_mortgage(10 * 1000 * 1000, 0.13);
-    payment_rent_flat(45 * 1000, 70 * 1000);
-    calculate(1.3);
-    print2();
+void alice_savings(const int month, const int year) {
+    if (alice.balance < 0) {
+        remittance = -alice.balance;
+    //    printf(" %lld \n", remittance);
+        if (alice.savings >= remittance) {
+            alice.savings -= remittance;
+            alice.balance = 0;
+        }
+        else {
+            alice.balance -= alice.savings;
+            alice.savings = 0;
+       //     printf(" %lld \n", alice.savings);
+        }
+    }
+    else {
+        alice.savings += alice.balance;
+        alice.balance = 0;
+    }
 }
 
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или     меню "Отладка" > "Запустить отладку"
+void alice_deposit(double deposit) {
+    alice.savings += alice.savings * deposit / 12;
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+
+void alice_food() {
+    alice.balance -= food;
+}
+
+
+void alice_cosmetic(const int month) {
+    if (month % 5 == 0) alice.balance -= cosmetic;
+}
+
+
+void alice_cat(const int month) {
+    if (month % 3 == 0) alice.balance -= cat_food;
+}
+
+
+void alice_communal_flat() {
+    alice.balance -= communal_flat;
+}
+
+
+void alice_clothes(const int month) {
+    if (month % 2 == 0) alice.balance -= clothes;
+}
+
+
+void alice_relaxation(const int month) {
+    if (month % 6 == 0) alice.balance -= relaxation;
+}
+
+
+void alice_hospital(const int month) {
+    if (month == 6 || month == 12) {
+        alice.balance -= hospital;
+    }
+}
+
+
+//                  BOB                  //
+
+
+void payment_rent_flat(RUB first_flat, RUB second_flat) {
+    second_flat_cost = second_flat;
+    bob.payment = first_flat;
+}
