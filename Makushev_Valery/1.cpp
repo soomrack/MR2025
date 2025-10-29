cb7420c1d358dde25297f714b73b2b6a8d5ec405
Slepec <makushevvs05@gmail.com>
2025-10-29 14:38:19 +0300

    Add files via upload

--- Makushev_Valery/1.cpp
+++ Makushev_Valery/1.cpp
@@ -5,10 +5,12 @@ typedef int RUB;
 int year = 2025;
 int month = 9;
 float inflation = 1.01; // за месяц
+float deposit_rate = 1.17; // за год
+int year_of_purchase;
+int month_of_purchase;
+int apartment_bought;
 
-
-struct person
-{
+struct Person {
     RUB bank_account;
     RUB salary;
     RUB food;
@@ -17,63 +19,59 @@ struct person
     RUB other_expenses;
     RUB rent;
     RUB mortgage;
+    RUB deposit;
+    RUB cost_of_the_apartment;
+    RUB deposit_account;
 };
 
 
-struct person Vlad;
+struct Person Vlad;
 
 
-void Vlad_salary()
-{
+void Vlad_salary(){
     if (month == 10)
     {
-        Vlad.salary *= 1.09; // indexation
+        Vlad.salary *= 1.09; // индексация
     }
 
     if (year == 2030 && month == 3) {
-        Vlad.salary *= 1.5;  // promotion
+        Vlad.salary *= 1.5;  // премия
     }
 
     Vlad.bank_account += Vlad.salary;
 }
 
 
-void Vlad_food()
-{
+void Vlad_food(){
     Vlad.bank_account -= Vlad.food;
     Vlad.food *= inflation;
 }
 
 
-void Vlad_clothers()
-{
+void Vlad_clothers(){
     Vlad.bank_account -= Vlad.clothers;
     Vlad.clothers *= inflation;
 }
 
 
-void Vlad_car()
-{
+void Vlad_car(){
     Vlad.bank_account -= Vlad.car;
     Vlad.car *= inflation;
 }
 
 
-void Vlad_mortgage()
-{
+void Vlad_mortgage(){
     Vlad.bank_account -= Vlad.mortgage;
 }
 
 
-void Vlad_other_expenses()
-{
+void Vlad_other_expenses(){
     Vlad.bank_account -= Vlad.other_expenses;
     Vlad.other_expenses *= inflation;
 }
 
 
-void Vlad_simulation()
-{
+void Vlad_simulation(){
 
     while (!(year == 2045 && month == 10))
     {
@@ -92,16 +90,13 @@ void Vlad_simulation()
         }
 
     }
-    year = 2025;
-    month = 9;
 
 }
 
 
-void Vlad_start()
-{
-    Vlad.bank_account = 100*1000;
-    Vlad.salary = 100*1000;
+void Vlad_start(){
+    Vlad.bank_account = 100 * 1000;
+    Vlad.salary = 100 * 1000;
     Vlad.food = 20000;
     Vlad.clothers = 5000;
     Vlad.car = 15000;
@@ -110,54 +105,49 @@ void Vlad_start()
 }
 
 
-void Vlad_print_results()
-{
-    printf("Vlad capital = %d\n\n", Vlad.bank_account);
+void Vlad_print_results(){
+    printf("Vlad financial status = %d RUB\n", Vlad.bank_account);
 }
 
 
-struct person Slava;
+struct Person Slava;
 
 
-void Slava_salary()
-{
+void Slava_salary(){
     if (month == 10)
     {
-        Slava.salary *= 1.09; // indexation
+        Slava.salary *= 1.09;
     }
 
-    if (year == 2030 && month == 3) {
-        Slava.salary *= 1.5;  // promotion
+    if (year == 2035 && month == 7)
+    {
+        Slava.salary *= 1.5;
+
     }
 
     Slava.bank_account += Slava.salary;
 }
 
 
-void Slava_food()
-{
+void Slava_food(){
     Slava.bank_account -= Slava.food;
     Slava.food *= inflation;
 }
 
 
-void Slava_clothers()
-{
+void Slava_clothers(){
     Slava.bank_account -= Slava.clothers;
     Slava.clothers *= inflation;
 }
 
 
-void Slava_car()
-{
+void Slava_car(){
     Slava.bank_account -= Slava.car;
     Slava.car *= inflation;
 }
 
-void Slava_rent()
-{
-    if (month == 1)
-    {
+void Slava_rent(){
+    if (month == 1) {
         Slava.rent *= 1.09; // годовая инфляция
     }
 
@@ -165,27 +155,46 @@ void Slava_rent()
 }
 
 
-void Slava_other_expenses()
-{
+void Slava_other_expenses(){
     Slava.bank_account -= Slava.other_expenses;
     Slava.other_expenses *= inflation;
 }
 
 
-void Slava_start()
-{
+void Slava_deposit(){
+    if (month == 1) {
+        Slava.deposit_account *= deposit_rate;
+    }
+}
+
+void SLava_cost_of_the_apartment(){
+    Slava.cost_of_the_apartment *= inflation;
+}
+void SLava_purchase() {
+    if (!apartment_bought && (Slava.cost_of_the_apartment < (Slava.deposit_account + Slava.bank_account))) {
+        year_of_purchase = year;
+        month_of_purchase = month;
+        apartment_bought = 1;
+    }
+}
+
+void Slava_start(){
     Slava.bank_account = 100 * 1000;
     Slava.salary = 100 * 1000;
     Slava.food = 20000;
     Slava.clothers = 5000;
-    Slava.car = 10000;
+    Slava.car = 15000;
     Slava.rent = 20000;
     Slava.other_expenses = 5000;
+    Slava.deposit_account = 1000 * 1000;
+    Slava.cost_of_the_apartment = 3.5 * 1000 * 1000;
+    year = 2025;
+    month = 9;
+
 }
 
 
-void Slava_simulation()
-{
+void Slava_simulation(){
 
     while (!(year == 2045 && month == 10))
     {
@@ -195,6 +204,9 @@ void Slava_simulation()
         Slava_car();
         Slava_rent();
         Slava_other_expenses();
+        Slava_deposit();
+        SLava_cost_of_the_apartment();
+        SLava_purchase();
 
         month++;
         if (month == 13)
@@ -208,13 +220,13 @@ void Slava_simulation()
 }
 
 
-void Slava_print_results()
-{
-    printf("Slava capital = %d\n\n", Slava.bank_account);
+void Slava_print_results(){
+    printf("Slava financial status = %d RUB\n", Slava.deposit_account + Slava.bank_account);
+    printf("cost_of_the_apartment = %d RUB\n", Slava.cost_of_the_apartment);
+    printf("Slava can buy an apartment in the %d month of %d \n", month_of_purchase, year_of_purchase);
 }
  
-int main()
-{
+int main(){
     Vlad_start();
     Vlad_simulation();
     Vlad_print_results();
