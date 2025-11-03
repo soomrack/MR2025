9e617380787e5788d41b0c0b08410daa58070ab2
Lizaveta-han <lizavetafadeeva.lizaf@gmail.com>
2025-11-03 15:17:03 +0300

    Update task2_line.ino
    fixes

--- Fadeeva_Elizaveta/task2_line/task2_line.ino
+++ Fadeeva_Elizaveta/task2_line/task2_line.ino
@@ -1,4 +1,3 @@
-// Пины
 #define MOTOR_L_PWM 6
 #define MOTOR_L_DIR 7
 #define MOTOR_R_PWM 5
@@ -7,104 +6,79 @@
 #define SENSOR_L A0
 #define SENSOR_R A1
 
+#define spiralInc 10             
+#define spiralInt 100           
+
+
 // Коэффициенты ПД-регулятора
-float Kp = 5;
-float Kd = 2;
-int baseSpeed = 150;
+float Kp = 3;
+float Kd = 1;
+int baseSpeed = 140;
 int searchSpeed = 120;
 
-// Переменные для калибровки 
-int l_white, r_white, l_black, r_black;
-int l_threshold, r_threshold;
-int l_minVal = 1023, l_maxVal = 0;
-int r_minVal = 1023, r_maxVal = 0;
+// Переменные (для калибровки и другие)
+int r_white, r_black, r_threshold;
+int l_white, l_black, l_threshold;
+int l_min = 1023, l_max = 0;
+int r_min = 1023, r_max = 0;
 
-// Другие переменные
 float lastError = 0; 
 bool systemActive = false;
-bool lastButtonState = LOW; 
+bool buttonState = LOW; 
+  
+unsigned long spiralTimer = 0;  
+int spiralStep = 0; 
+int lastDirect = 0;    
+bool spiralDirect = 0;
+bool lineFound = false;
 
 // Управление моторами 
 void setMotors(int left, int right) {
-    Serial.print("Motor L:"); Serial.print(left); Serial.print("Motor R:"); Serial.println(right);
     digitalWrite(MOTOR_L_DIR, left >= 0);
     digitalWrite(MOTOR_R_DIR, right >= 0);
     analogWrite(MOTOR_L_PWM, constrain(abs(left), 0, 255));
     analogWrite(MOTOR_R_PWM, constrain(abs(right), 0, 255));
 }
 
-// Усреднённое считывание аналогового сигнала 
-int readSmooth(int pin) {
+// Считывание аналогового сигнала с усреднением
+int readAnalog(int pin) {
     long total = 0;
     const char attempts = 17;
-    for (int i = 0; i < attempts; i++) total += analogRead(pin);
+    for (int i = 0; i < attempts; i++) {
+      total += analogRead(pin);}
     return total / attempts;
 }
 
 // Калибровка датчиков 
-void calibrate() {
+void calibrateDat() {
     Serial.println("Place on white and press button");
     while (digitalRead(BUTTON_PIN)) delay(10);
     while (!digitalRead(BUTTON_PIN)) delay(10);
 
-    l_white = readSmooth(SENSOR_L);
-    r_white = readSmooth(SENSOR_R);
+    l_white = readAnalog(SENSOR_L);
+    r_white = readAnalog(SENSOR_R);
 
     Serial.println("Place on black and press button");
     while (digitalRead(BUTTON_PIN)) delay(10);
     while (!digitalRead(BUTTON_PIN)) delay(10);
 
-    l_black = readSmooth(SENSOR_L);
-    r_black = readSmooth(SENSOR_R);
+    l_black = readAnalog(SENSOR_L);
+    r_black = readAnalog(SENSOR_R);
 
-    l_minVal = l_black;
-    l_maxVal = l_white;
-    r_minVal = r_black;
-    r_maxVal = r_white;
+    l_min = l_black;
+    l_max = l_white;
+    r_min = r_black;
+    r_max = r_white;
 
-    const float ratio = 0.1; // порог потери линии
-    l_threshold = l_black + (l_white - l_black) * ratio;
-    r_threshold = r_black + (r_white - r_black) * ratio;
-
-    Serial.println("Calibration completed:");
-    Serial.print("L: "); Serial.print(l_minVal); Serial.print(" , "); Serial.println(l_maxVal);
-    Serial.print("R: "); Serial.print(r_minVal); Serial.print(" , "); Serial.println(r_maxVal);
-    Serial.print("Thresholds: "); Serial.print(l_threshold); Serial.print(" , "); Serial.println(r_threshold);
-}
-
-// Проверка потряна ли ниния
-bool lineLost() {
-    int l_read = analogRead(SENSOR_L);
-    int r_read = analogRead(SENSOR_R);
-    return (l_read < l_threshold && r_read < r_threshold);
-}
-
-// Поиск линии при потере
-void findLine() {
-  Serial.println("Поиск линии");
-  bool isSearching = true;
-  unsigned long startTime = millis();
-
-  while (isSearching && millis() - startTime < 5000) {
-    if (lastError > 0) setMotors(searchSpeed, -searchSpeed);
-    else setMotors(-searchSpeed, searchSpeed);
-
-    if (!lineLost()) {
-      Serial.println("Line found");
-      isSearching = false;
-      return;
-    }
-  }
-
-  setMotors(0, 0);
-  Serial.println("Line lost");
-  isSearching = false;
+    const float corect = 0.1; 
+    l_threshold = l_black + (l_white - l_black) * corect;
+    r_threshold = r_black + (r_white - r_black) * corect;
 }
 
 // Следование по линии
-void followTrack() {
-    int l_val = map(analogRead(SENSOR_L), l_minVal, l_maxVal, 0, 100);
-    int r_val = map(analogRead(SENSOR_R), r_minVal, r_maxVal, 0, 100);
+void followLine() {
+    int l_val = map(analogRead(SENSOR_L), l_min, l_max, 0, 100);
+    int r_val = map(analogRead(SENSOR_R), r_min, r_max, 0, 100);
 
     float error =  l_val - r_val;
     float correction = Kp * error + Kd * (error - lastError);
@@ -114,6 +88,40 @@ void followTrack() {
     int rightPower = baseSpeed - correction;
 
     setMotors(leftPower, rightPower);
+}
+
+// Поиск линии при потере по спирали
+void findLine() {
+    bool isSearching = true;
+    unsigned long startTime = millis();
+    spiralStep = 0;
+
+    while (isSearching && millis() - startTime < 10000) {
+        if (millis() - spiralTimer > spiralInt) {
+            spiralStep += spiralInc;
+            spiralTimer = millis();
+        }
+
+        int l_val = constrain(searchSpeed - spiralStep, 0, 255);
+        int r_val = searchSpeed;
+
+        setMotors(l_val, r_val);
+    }
+
+    if (lineLost()) {
+        systemActive = false;
+    }
+
+    setMotors(0, 0);
+    delay(10);
+}
+
+// Проверка потряна ли линия
+bool lineLost() {
+    int l_read = readAnalog(SENSOR_L);
+    int r_read = readAnalog(SENSOR_R);
+
+    return (r_read < r_threshold && l_read < l_threshold);
 }
 
 // Инициализация
@@ -126,7 +134,7 @@ void setup() {
     pinMode(MOTOR_R_DIR, OUTPUT);
     pinMode(BUTTON_PIN, INPUT_PULLUP);
 
-    calibrate();
+    calibrateDat();
 
     Serial.println("Press button to start");
     while (digitalRead(BUTTON_PIN) == HIGH) delay(10);
@@ -137,17 +145,18 @@ void setup() {
 void loop() {
     bool btnState = digitalRead(BUTTON_PIN);
 
-    if (btnState == LOW && lastButtonState == HIGH) {
+    if (btnState == LOW && buttonState == HIGH) {
         systemActive = !systemActive;
         delay(80);
     }
-    lastButtonState = btnState;
+
+    buttonState = btnState;
 
     if (systemActive) {
         if (lineLost()){ 
             findLine();
          }
          else 
-        followTrack();
+        followLine();
     }
 }
