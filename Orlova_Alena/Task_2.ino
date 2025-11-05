e4f15e015e2188197248ba8483e6afc3fcf658bf
ArtemYurikov <artemyr2005@gmail.com>
2025-11-05 15:06:11 +0300

    last

--- Orlova_Alena/Task_2.ino
+++ Orlova_Alena/Task_2.ino
@@ -1,125 +1,127 @@
-const int Left_Sensor = A3;
-const int Right_Sensor = A2;
-const int Left_Motor_PWM = 6;
-const int Left_Motor_Dir = 7;
-const int Right_Motor_PWM = 5;
-const int Right_Motor_Dir = 4;
-const int Buzzer = 8;
+ const int Left_Sensor_Pin = A3;
+  const int Right_Sensor_Pin = A2;
+  const int Left_Motor_PWM_Pin = 6;
+  const int Left_Motor_Dir_Pin = 7;
+  const int Right_Motor_PWM_Pin = 5;
+  const int Right_Motor_Dir_Pin = 4;
+  const int Buzzer_Pin = 8;
 
-const int Black_Threshold = 700;
-const int Motor_Speed = 150;
-const int dMotor_Delta_Speed = 40;
+  const int Black_Threshold = 700;
+  const int Motor_Speed = 150;
+  const int dMotor_Delta_Speed = 40;
 
-unsigned long Lost_Time = 0;
-const unsigned long Search_Time = 4000;
-bool Is_Searching = false;
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(Left_Sensor, INPUT);
-  pinMode(Right_Sensor, INPUT);
-  pinMode(Buzzer, OUTPUT);
-  pinMode(Left_Motor_PWM, OUTPUT);
-  pinMode(Left_Motor_Dir, OUTPUT);
-  pinMode(Right_Motor_PWM, OUTPUT);
-  pinMode(Right_Motor_Dir, OUTPUT);
-}
+  unsigned long Lost_Time = 0;
+  const unsigned long Search_Time = 4000;
+  bool Is_Searching = false;
+  bool Is_waiting = false;
 
-void loop() {
-  int leftSensorValue = analogRead(Left_Sensor);
-  int rightSensorValue = analogRead(Right_Sensor);
-  bool isLeftLineBlack = leftSensorValue > Black_Threshold;
-  bool isRightLineBlack = rightSensorValue > Black_Threshold;
-
-  if (Is_Searching) {
-    search_line(isLeftLineBlack, isRightLineBlack);
-  } else {
-    follow_line(isLeftLineBlack, isRightLineBlack);
+  void setup() {
+    Serial.begin(9600);
+    pinMode(Left_Sensor_Pin, INPUT);
+    pinMode(Right_Sensor_Pin, INPUT);
+    pinMode(Buzzer_Pin, OUTPUT);
+    pinMode(Left_Motor_PWM_Pin, OUTPUT);
+    pinMode(Left_Motor_Dir_Pin, OUTPUT);
+    pinMode(Right_Motor_PWM_Pin, OUTPUT);
+    pinMode(Right_Motor_Dir_Pin, OUTPUT);
   }
-}
 
-void beep(int duration) {
-  tone(Buzzer, 500, duration);
-}
+  void beep(int duration) {
+    tone(Buzzer_Pin, 500, duration);
+  }
 
-void follow_line(bool isLeftLineBlack, bool isRightLineBlack) {
-  if (isLeftLineBlack && isRightLineBlack) {
-    move_Forward();
-  } else if (!isLeftLineBlack && isRightLineBlack) {
-    turn_Left();
-  } else if (isLeftLineBlack && !isRightLineBlack) {
-    turn_Right();
-  } else {
-    if (!Is_Searching) {
-      Is_Searching = true;
-      Lost_Time = millis();
-      beep(200);
+  void follow_line(bool is_Left_Line_Black, bool is_Right_Line_Black) {
+    if (is_Left_Line_Black && is_Right_Line_Black) {
+      move_Forward();
+    } else if (!is_Left_Line_Black && is_Right_Line_Black) {
+      turn_Left();
+    } else if (is_Left_Line_Black && !is_Right_Line_Black) {
+      turn_Right();
+    } else {
+      if (!Is_Searching) {
+        Is_Searching = true;
+        Lost_Time = millis();
+        beep(200);
+      }
     }
   }
-}
 
-void search_line(bool isLeftLineBlack, bool isRightLineBlack) {
-  if (isLeftLineBlack || isRightLineBlack) {
-    Is_Searching = false;
-    beep(100);
-    return;
+  void search_line(bool is_Left_Line_Black, bool is_Right_Line_Black) {
+    if (is_Left_Line_Black || is_Right_Line_Black) {
+      Is_Searching = false;
+      beep(100);
+      return;
+    }
+
+    unsigned long current_Time = millis() - Lost_Time;
+    
+    if (current_Time % 2000 < 2000) {
+      move_Find_Right();
+    } else {
+      move_Find_Left();
+    }
+
+    if (millis() - Lost_Time > Search_Time) {
+      Is_waiting = true;
+    }
   }
 
-  if (millis() - Lost_Time > Search_Time) {
+  void move_Forward() {
+    analogWrite(Left_Motor_PWM_Pin, Motor_Speed);
+    digitalWrite(Left_Motor_Dir_Pin, HIGH);
+    analogWrite(Right_Motor_PWM_Pin, Motor_Speed);
+    digitalWrite(Right_Motor_Dir_Pin, HIGH);
+  }
+
+  void turn_Left() {
+    analogWrite(Left_Motor_PWM_Pin, Motor_Speed);
+    digitalWrite(Left_Motor_Dir_Pin, LOW);
+    analogWrite(Right_Motor_PWM_Pin, Motor_Speed + dMotor_Delta_Speed);
+    digitalWrite(Right_Motor_Dir_Pin, HIGH);
+  }
+
+  void turn_Right() {
+    analogWrite(Left_Motor_PWM_Pin, Motor_Speed + dMotor_Delta_Speed);
+    digitalWrite(Left_Motor_Dir_Pin, HIGH);
+    analogWrite(Right_Motor_PWM_Pin, Motor_Speed);
+    digitalWrite(Right_Motor_Dir_Pin, LOW);
+  }
+
+  void Stop_Moving() {
+    digitalWrite(Left_Motor_PWM_Pin, LOW);
+    digitalWrite(Left_Motor_Dir_Pin, LOW);
+    digitalWrite(Right_Motor_PWM_Pin, LOW);
+    digitalWrite(Right_Motor_Dir_Pin, LOW);
+  }
+
+  void move_Find_Right() {
+    analogWrite(Left_Motor_PWM_Pin, 200);
+    digitalWrite(Left_Motor_Dir_Pin, HIGH);
+    analogWrite(Right_Motor_PWM_Pin, 0);
+    digitalWrite(Right_Motor_Dir_Pin, LOW);
+  }
+
+  void move_Find_Left() {
+    analogWrite(Left_Motor_PWM_Pin, 0);
+    digitalWrite(Left_Motor_Dir_Pin, LOW);
+    analogWrite(Right_Motor_PWM_Pin, 200);
+    digitalWrite(Right_Motor_Dir_Pin, HIGH);
+  }
+
+  void loop() {
+    int left_Sensor_Value = analogRead(Left_Sensor_Pin);
+    int right_Sensor_Value = analogRead(Right_Sensor_Pin);
+    bool is_Left_Line_Black = left_Sensor_Value > Black_Threshold;
+    bool is_Right_Line_Black = right_Sensor_Value > Black_Threshold;
+
+    if (Is_Searching) {
+      search_line(is_Left_Line_Black, is_Right_Line_Black);
+    } else {
+      follow_line(is_Left_Line_Black, is_Right_Line_Black);
+    }
+    if (Is_waiting) {
     Stop_Moving();
-    while(true) {
-      digitalWrite(Buzzer, HIGH);
+    beep(Search_Time);
     }
   }
-
-  unsigned long currentTime = millis() - Lost_Time;
-  
-  if (currentTime % 2000 < 2000) {
-    move_Find_Right();
-  } else {
-    move_Find_Left();
-  }
-}
-
-
-void move_Forward() {
-  analogWrite(Left_Motor_PWM, Motor_Speed);
-  digitalWrite(Left_Motor_Dir, HIGH);
-  analogWrite(Right_Motor_PWM, Motor_Speed);
-  digitalWrite(Right_Motor_Dir, HIGH);
-}
-
-void turn_Left() {
-  analogWrite(Left_Motor_PWM, Motor_Speed);
-  digitalWrite(Left_Motor_Dir, LOW);
-  analogWrite(Right_Motor_PWM, Motor_Speed + dMotor_Delta_Speed);
-  digitalWrite(Right_Motor_Dir, HIGH);
-}
-
-void turn_Right() {
-  analogWrite(Left_Motor_PWM, Motor_Speed + dMotor_Delta_Speed);
-  digitalWrite(Left_Motor_Dir, HIGH);
-  analogWrite(Right_Motor_PWM, Motor_Speed);
-  digitalWrite(Right_Motor_Dir, LOW);
-}
-
-void Stop_Moving() {
-  digitalWrite(Left_Motor_PWM, LOW);
-  digitalWrite(Left_Motor_Dir, LOW);
-  digitalWrite(Right_Motor_PWM, LOW);
-  digitalWrite(Right_Motor_Dir, LOW);
-}
-
-void move_Find_Right() {
-  analogWrite(Left_Motor_PWM, 200);
-  digitalWrite(Left_Motor_Dir, HIGH);
-  analogWrite(Right_Motor_PWM, 0);
-  digitalWrite(Right_Motor_Dir, LOW);
-}
-
-void move_Find_Left() {
-  analogWrite(Left_Motor_PWM, 0);
-  digitalWrite(Left_Motor_Dir, LOW);
-  analogWrite(Right_Motor_PWM, 200);
-  digitalWrite(Right_Motor_Dir, HIGH);
-}
