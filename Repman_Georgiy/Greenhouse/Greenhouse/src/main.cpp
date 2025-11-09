10fab464e1f31a856d664e1efdb70f05ea849c45
repman <repman.ga@mail.ru>
2025-11-09 17:24:03 +0300

    Added greenhouse project template
    1) Developed greenhouse architecture;
    2) Find every sensor for greenhouse;

---
+++ Repman_Georgiy/Greenhouse/Greenhouse/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <DHT.h>
+#include <Adafruit_Sensor.h>
+/* Simple greenhouse project version beta1.0*/
+/* I'm to lazy to make this project for tomorrow*/
+/* : ( */
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
